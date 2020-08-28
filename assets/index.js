72d9843ed13b5b7e58a3131df6e69d1184b71923
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-27 20:04:23 -0600

    merging master

--- assets/index.js
+++ assets/index.js
@@ -5,3 +5,34 @@
 // Scope API and determine additional functionality 
 // create for loop to create x number of cards to populate
 // Display synopsis and critical data (RT, IMDb) below when image clicked
+
+
+var newSettings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+		"x-rapidapi-key": "3a6a7033a5msh785a852c65c15c2p1ed117jsne310d6bcef70"
+	}
+}
+
+$.ajax(newSettings).done(function (newResponse) {
+	console.log(newResponse);
+});
+
+var expireSettings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Aexp%3AUS&t=ns&st=adv&p=1",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+		"x-rapidapi-key": "3a6a7033a5msh785a852c65c15c2p1ed117jsne310d6bcef70"
+	}
+}
+
+$.ajax(expireSettings).done(function (expireResponse) {
+	console.log(expireResponse);
+});
