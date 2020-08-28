145a773924b1089c5866921205e653c5ec0640fd
Tony Rivas <tonyjrivas@gmail.com>
2020-08-27 19:53:13 -0600

    added rudimentary pseudocode for js

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+// Set API in carousel
+// Function that calls 2nd API so both can occupy same carousel
+// Scope API and determine additional functionality 
+// create for loop to create x number of cards to populate
+// Display synopsis and critical data (RT, IMDb) below when image clicked
