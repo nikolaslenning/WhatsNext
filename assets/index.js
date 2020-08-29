72927991322f4bd50fe8c97a97a80302d2e67014
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-29 10:42:15 -0600

    deleted duplicate js index

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,10 @@
 
 // Set API in carousel
+
+
+
+
+
 // Function that calls 2nd API so both can occupy same carousel
 // Set array to carousel
 // Scope API and determine additional functionality 
@@ -18,21 +23,26 @@ var newSettings = {
 	}
 }
 
-$.ajax(newSettings).done(function (newResponse) {
-	console.log(newResponse);
-});
 
-var expireSettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Aexp%3AUS&t=ns&st=adv&p=1",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
-		"x-rapidapi-key": "3a6a7033a5msh785a852c65c15c2p1ed117jsne310d6bcef70"
-	}
+function pull() {
+	$.ajax(newSettings).done(function (newResponse) {
+		console.log(newResponse.ITEMS[0].rating);
+		$(".p1").text("Rating: " + newResponse.ITEMS[0].rating)
+		console.log(newResponse);
+	});
 }
+	var expireSettings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Aexp%3AUS&t=ns&st=adv&p=1",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+			"x-rapidapi-key": "3a6a7033a5msh785a852c65c15c2p1ed117jsne310d6bcef70"
+		}
+	}
 
-$.ajax(expireSettings).done(function (expireResponse) {
-	console.log(expireResponse);
-});
+	// pull();
+	// $.ajax(expireSettings).done(function (expireResponse) {
+	// 	console.log(expireResponse);
+pull();
